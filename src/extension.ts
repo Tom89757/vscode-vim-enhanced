39ee0b39993d3e040fa2d9f7731e8d01745f1048
Tom89757 <2689980244@qq.com>
2024-11-21 18:29:19 +0800

    参照教程调试成功

--- src/extension.ts
+++ src/extension.ts
@@ -16,7 +16,7 @@ export function activate(context: vscode.ExtensionContext) {
 	const disposable = vscode.commands.registerCommand('vscode-vim-enhanced.helloWorld', () => {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from vscode-vim-enhanced!');
+		vscode.window.showInformationMessage('Hello World from vim!');
 	});
 
 	context.subscriptions.push(disposable);
