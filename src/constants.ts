021e1cd597dfb93efd72245f79d133ab583bd180
Tom89757 <2689980244@qq.com>
2024-12-11 19:02:11 +0800

    添加颜色设置

--- src/constants.ts
+++ src/constants.ts
@@ -2,3 +2,5 @@ export const CHAR_PRIMARY_COLOR = "red";
 // export const CHAR_SECONDARY_COLOR = "green";
 export const CHAR_SECONDARY_COLOR = "#ffb400";
 export const CHAR_FONTWEIGHT = "400";
+export const CHAR_VISUALLINE_BELOW = "red";
+export const CHAR_VISUALLINE_ABOVE = "green";
--- src/decoration.ts
+++ src/decoration.ts
@@ -3,6 +3,8 @@ import {
   CHAR_FONTWEIGHT,
   CHAR_PRIMARY_COLOR,
   CHAR_SECONDARY_COLOR,
+  CHAR_VISUALLINE_ABOVE,
+  CHAR_VISUALLINE_BELOW,
 } from "./constants";
 
 let charDecoration: vscode.TextEditorDecorationType | undefined;
@@ -46,36 +48,41 @@ export const getCharDecorationSecondColor = (
   return charDecorationSecondColor;
 };
 
-
 // Define decoration types for relative line numbers above and below
-export const relativeLineNumberAboveDecoration = vscode.window.createTextEditorDecorationType({
-  after: {
-    contentText: "",
-    margin: "0 0 0 5px",
-    // Additional styling if needed
-  },
-  isWholeLine: true,
-});
+export const relativeLineNumberAboveDecoration =
+  vscode.window.createTextEditorDecorationType({
+    after: {
+      contentText: "",
+      margin: "0 0 0 5px",
+      // Additional styling if needed
+    },
+    isWholeLine: true,
+  });
 
-export const relativeLineNumberBelowDecoration = vscode.window.createTextEditorDecorationType({
-  after: {
-    contentText: "",
-    margin: "0 0 0 5px",
-    // Additional styling if needed
-  },
-  isWholeLine: true,
-});
+export const relativeLineNumberBelowDecoration =
+  vscode.window.createTextEditorDecorationType({
+    after: {
+      contentText: "",
+      margin: "0 0 0 5px",
+      // Additional styling if needed
+    },
+    isWholeLine: true,
+  });
 
 export class DecorationConfig {
   public firstColor: string;
   public secondColor: string;
   public fontWeight: string;
   public underline: boolean;
+  public visualLineAbove: string;
+  public visualLineBelow: string;
   constructor() {
     this.firstColor = CHAR_PRIMARY_COLOR;
     this.secondColor = CHAR_SECONDARY_COLOR;
     this.fontWeight = CHAR_FONTWEIGHT;
     this.underline = true;
+    this.visualLineAbove = CHAR_VISUALLINE_ABOVE;
+    this.visualLineBelow = CHAR_VISUALLINE_BELOW;
   }
 }
 
@@ -85,10 +92,14 @@ export const updateDecorationConfig = () => {
   const primaryColor = settings.get("vscodeVimEnhanced.charPrimaryColor");
   const secondaryColor = settings.get("vscodeVimEnhanced.charSecondaryColor");
   const underline = settings.get("vscodeVimEnhanced.enableUnderline");
+  const visualLineAbove = settings.get("vscodeVimEnhanced.visualLineAbove");
+  const visualLineBelow = settings.get("vscodeVimEnhanced.visualLineBelow");
   decorationConfig.firstColor = primaryColor as string;
   decorationConfig.secondColor = secondaryColor as string;
   decorationConfig.fontWeight = fontWeight as string;
   decorationConfig.underline = underline as boolean;
+  decorationConfig.visualLineAbove = visualLineAbove as string;
+  decorationConfig.visualLineBelow = visualLineBelow as string;
 };
 
 export const decorationConfig = new DecorationConfig();
